031deac1eb8f5ed30c838c67c67d612183575116
Spark <vraymond@sparkfusiondigital.com>
2025-08-09 20:49:55 -0400

    Update page.tsx

--- src/app/company/[id]/page.tsx
+++ src/app/company/[id]/page.tsx
@@ -1,13 +1,1 @@
-// src/app/company/[id]/page.tsx
-import CompanyDrawer from '../../../components/drawer/CompanyDrawer';
-
-export default function CompanyPage({ params }: { params: { id: string } }) {
-  const { id } = params; // company_id from the URL
-  return (
-    <div className="min-h-screen bg-white">
-      <div className="max-w-screen-xl mx-auto px-4 md:px-6 py-6">
-        <CompanyDrawer companyId={id} plan="trial" />
-      </div>
-    </div>
-  );
-}
+import CompanyDrawer from '../../../components/CompanyDrawer';
