d8249ae6d146d9257e9939a2609e9e994ed44cbb
Spark <vraymond@sparkfusiondigital.com>
2025-08-09 22:21:17 -0400

    Update page.tsx

--- src/app/company/[id]/page.tsx
+++ src/app/company/[id]/page.tsx
@@ -1,11 +1,8 @@
 import CompanyDrawer from '../../../components/CompanyDrawer';
 
-type PageProps = { params: { id: string } };
-
-export default function CompanyPage({ params }: PageProps) {
+export default function CompanyPage({ params }: { params: { id: string } }) {
   return (
     <div className="min-h-screen p-4 md:p-6">
-      {/* plan='trial' is fine; Add to CRM flips to premium */}
       <CompanyDrawer companyId={params.id} plan="trial" />
     </div>
   );
