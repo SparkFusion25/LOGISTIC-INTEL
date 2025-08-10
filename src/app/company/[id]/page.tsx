1ef5492a6ea012422335e50d458116318257ecd2
Spark <vraymond@sparkfusiondigital.com>
2025-08-09 21:34:09 -0400

    Update page.tsx

--- src/app/company/[id]/page.tsx
+++ src/app/company/[id]/page.tsx
@@ -5,7 +5,7 @@ type PageProps = { params: { id: string } };
 export default function CompanyPage({ params }: PageProps) {
   return (
     <div className="min-h-screen p-4 md:p-6">
-      {/* Use trial for now; Add to CRM flips to premium */}
+      {/* plan='trial' is fine; Add to CRM flips to premium */}
       <CompanyDrawer companyId={params.id} plan="trial" />
     </div>
   );
